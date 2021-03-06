0ab4917accb262a9fa153976610a3eb2aa211bd3
ztulnnx <jiabao52002443@163.com>
2021-03-06 14:00:31 +0800

    堆

--- src/com/zhl/test/util/heap/Heap.java
+++ src/com/zhl/test/util/heap/Heap.java
@@ -7,13 +7,20 @@ package com.zhl.test.util.heap;
  */
 public class Heap {
 	
+	public static final int HEAD_MAX = 0;
+	public static final int HEAD_MIN = 1;
+	
 	public static void main(String[] args) {
 		int[] A = {49,38,52,44,81,97,76,13,27,65};
 		print(A,"原始堆");
-		buildMaxHeap(A);
+		buildHeap(A,HEAD_MAX);
 		print(A,"最大堆");
-		heapSort(A);
-		print(A,"堆排序");
+		heapSort(A,HEAD_MAX);
+		print(A,"最大堆排序");
+		buildHeap(A,HEAD_MIN);
+		print(A,"最小堆");
+		heapSort(A,HEAD_MIN);
+		print(A,"最小堆排序");
 	}
 	
 	/**
@@ -28,51 +35,67 @@ public class Heap {
 		}
 		System.out.println("");
 	}
+	
 	/**
 	 * 堆排序
 	 * @param A
+	 * @param type
 	 */
-	public static void heapSort(int[] A){
-		buildMaxHeap(A);
+	public static void heapSort(int[] A,int type){
+		buildHeap(A,type);
 		int heapSize = A.length - 1;
 		for (int i = heapSize; i >= 0; i--) {
 			swap(A, 0, i);
-			maxHeapify(A, 0, i - 1);
+			heapify(A, 0, i - 1,type);
 		}
 	}
 	
 	/**
-	 * 得到最大堆
-	 * @param A
+	 * 根据type得到最大/最小堆
+	 * @param A	堆
+	 * @param type	MAX,MIN
 	 */
-	public static void buildMaxHeap(int[] A){
+	public static void buildHeap(int[] A,int type){
 		int heapSize = A.length - 1;
 		for (int i = heapSize / 2; i >= 0;i--) {
-			maxHeapify(A, i, heapSize);
+			heapify(A, i, heapSize, type);
 		}
 	}
 	
 	/**
-	 * 最大堆
-	 * @param A	最大堆数组
-	 * @param i	索引
+	 * 最大/最小堆
+	 * @param A 堆
+	 * @param i	下标
+	 * @param heapSize	堆长度
+	 * @param type	MAX,MIN
 	 */
-	public static void maxHeapify(int[] A,int i,int heapSize){
+	public static void heapify(int[] A,int i,int heapSize,int type){
 		int l = left(i);	//得到左孩子下标
 		int r = right(i);	//得到右孩子下标
 		int largest = i;
-		//如果左孩子val比A[i]的val大,则将其下标存储在largest中
-		if (l <= heapSize && A[l] > A[i]) {
-			largest = l;
-		}
-		//如果右孩子val比A[i]的val大,则将其下标存储在largest中
-		if (r <= heapSize && A[r] > A[largest]) {
-			largest = r;
+		if (type == HEAD_MAX) {
+			//如果左孩子val比A[i]的val大,则将其下标存储在largest中
+			if (l <= heapSize && A[l] > A[i]) {
+				largest = l;
+			}
+			//如果右孩子val比A[i]的val大,则将其下标存储在largest中
+			if (r <= heapSize && A[r] > A[largest]) {
+				largest = r;
+			}
+		} else {
+			//如果左孩子val比A[i]的val大,则将其下标存储在largest中
+			if (l <= heapSize && A[l] < A[i]) {
+				largest = l;
+			}
+			//如果右孩子val比A[i]的val大,则将其下标存储在largest中
+			if (r <= heapSize && A[r] < A[largest]) {
+				largest = r;
+			}
 		}
 		//如果下标i对应的元素不为最大,则不符合最大堆性质,交换i和largest的元素
 		if (largest != i) {
 			swap(A, i, largest);
-			maxHeapify(A, largest, heapSize);
+			heapify(A, largest, heapSize,type);
 		}
 	}
 	
