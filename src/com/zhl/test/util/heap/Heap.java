213f2f5e930ee6540bb9111d02c6b96a68f2b7bc
ztulnnx <jiabao52002443@163.com>
2021-03-09 11:56:08 +0800

    堆

--- src/com/zhl/test/util/heap/Heap.java
+++ src/com/zhl/test/util/heap/Heap.java
@@ -5,110 +5,102 @@ package com.zhl.test.util.heap;
  * @author zhanghanlin
  *
  */
-public class Heap {
-	
+public class Heap {	
 	public static final int HEAD_MAX = 0;
 	public static final int HEAD_MIN = 1;
 	
-	public static void main(String[] args) {
-		int[] A = {49,38,52,44,81,97,76,13,27,65};
-		print(A,"原始堆");
-		buildHeap(A,HEAD_MAX);
-		print(A,"最大堆");
-		heapSort(A,HEAD_MAX);
-		print(A,"最大堆排序");
-		buildHeap(A,HEAD_MIN);
-		print(A,"最小堆");
-		heapSort(A,HEAD_MIN);
-		print(A,"最小堆排序");
+	public int[] src;
+	public int size;
+	
+	public Heap() {
+		super();
+	}
+	
+	public Heap(int[] src){
+		this.src = src;
+		this.size = src.length;
 	}
 	
 	/**
 	 * 打印元素
-	 * @param A
 	 * @param s info
 	 */
-	public static void print(int[] A,String s){
+	public void print(String s){
 		System.out.print(s + " > ");
-		for (int i = 0; i < A.length; i++) {
-			System.out.print(A[i] + " ");
+		for (int i = 0; i < size; i++) {
+			System.out.print(src[i] + " ");
 		}
 		System.out.println("");
 	}
 	
 	/**
 	 * 堆排序
-	 * @param A
 	 * @param type
 	 */
-	public static void heapSort(int[] A,int type){
-		buildHeap(A,type);
-		int heapSize = A.length - 1;
-		for (int i = heapSize; i >= 0; i--) {
-			swap(A, 0, i);
-			heapify(A, 0, i - 1,type);
+	public void headSort(int type){
+		buildHeap(type);
+		for (int i = size - 1; i >= 0; i--) {
+			swap(0, i);
+			heapify(0, i - 1,type);
 		}
 	}
 	
 	/**
 	 * 根据type得到最大/最小堆
-	 * @param A	堆
 	 * @param type	MAX,MIN
 	 */
-	public static void buildHeap(int[] A,int type){
-		int heapSize = A.length - 1;
+	public void buildHeap(int type){
+		int heapSize = size - 1;
 		for (int i = heapSize / 2; i >= 0;i--) {
-			heapify(A, i, heapSize, type);
+			heapify(i, heapSize, type);
 		}
 	}
 	
 	/**
 	 * 最大/最小堆
-	 * @param A 堆
 	 * @param i	下标
 	 * @param heapSize	堆长度
 	 * @param type	MAX,MIN
 	 */
-	public static void heapify(int[] A,int i,int heapSize,int type){
+	public void heapify(int i,int heapSize,int type){
 		int l = left(i);	//得到左孩子下标
 		int r = right(i);	//得到右孩子下标
 		int largest = i;
 		if (type == HEAD_MAX) {
-			//如果左孩子val比A[i]的val大,则将其下标存储在largest中
-			if (l <= heapSize && A[l] > A[i]) {
+			//如果左孩子val比src[i]的val大,则将其下标存储在largest中
+			if (l <= heapSize && src[l] > src[i]) {
 				largest = l;
 			}
-			//如果右孩子val比A[i]的val大,则将其下标存储在largest中
-			if (r <= heapSize && A[r] > A[largest]) {
+			//如果右孩子val比src[i]的val大,则将其下标存储在largest中
+			if (r <= heapSize && src[r] > src[largest]) {
 				largest = r;
 			}
 		} else {
-			//如果左孩子val比A[i]的val大,则将其下标存储在largest中
-			if (l <= heapSize && A[l] < A[i]) {
+			//如果左孩子val比src[i]的val大,则将其下标存储在largest中
+			if (l <= heapSize && src[l] < src[i]) {
 				largest = l;
 			}
-			//如果右孩子val比A[i]的val大,则将其下标存储在largest中
-			if (r <= heapSize && A[r] < A[largest]) {
+			//如果右孩子val比src[i]的val大,则将其下标存储在largest中
+			if (r <= heapSize && src[r] < src[largest]) {
 				largest = r;
 			}
 		}
 		//如果下标i对应的元素不为最大,则不符合最大堆性质,交换i和largest的元素
 		if (largest != i) {
-			swap(A, i, largest);
-			heapify(A, largest, heapSize,type);
+			swap(i, largest);
+			heapify(largest, heapSize,type);
 		}
 	}
 	
 	/**
 	 * 交换i和largest的元素
-	 * @param A
 	 * @param i
 	 * @param largest
 	 */
-	public static void swap(int[] A,int i,int largest){
-		int temp = A[largest];
-		A[largest] = A[i];
-		A[i] = temp;
+	public void swap(int i,int largest){
+		int temp = src[largest];
+		src[largest] = src[i];
+		src[i] = temp;
 	}
 	
 	/**
@@ -116,7 +108,7 @@ public class Heap {
 	 * @param i
 	 * @return
 	 */
-	public static int parent(int i){
+	public int parent(int i){
 		return i/2;
 	}
 	
@@ -125,7 +117,7 @@ public class Heap {
 	 * @param i
 	 * @return
 	 */
-	public static int left(int i){
+	public int left(int i){
 		return 2 * i;
 	}
 	
@@ -134,7 +126,21 @@ public class Heap {
 	 * @param i
 	 * @return
 	 */
-	public static int right(int i){
+	public int right(int i){
 		return 2 * i + 1;
+	}
+	
+	public static void main(String[] args) {
+		int[] src = {49,38,52,44,81,97,76,13,27,65};
+		Heap heap = new Heap(src);
+		heap.print("原始堆");
+		heap.buildHeap(HEAD_MAX);
+		heap.print("最大堆");
+		heap.headSort(HEAD_MAX);
+		heap.print("最大堆排序");
+		heap.buildHeap(HEAD_MIN);
+		heap.print("最小堆");
+		heap.headSort(HEAD_MIN);
+		heap.print("最小堆排序");
 	}
 }
