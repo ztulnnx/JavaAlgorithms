f0b0539ae1ba991f8a63891536743a3612c9ebaa
ztulnnx <jiabao52002443@163.com>
2021-03-17 11:31:09 +0800

    快速排序 - 随机抽样

--- src/com/zhl/test/util/sort/QuickSort.java
+++ src/com/zhl/test/util/sort/QuickSort.java
@@ -43,12 +43,46 @@ public class QuickSort {
 		return i;
 	}
 	
+	/**
+	 * 快速排序随机化版本
+	 * @param p
+	 * @param r
+	 */
+	public static void randomSort(int p, int r){
+		if (p < r) {
+			int q = randomPartition(p, r);
+			randomPartition(p, q - 1);
+			randomPartition(q + 1, r);
+		}
+	}
+	
+	/**
+	 * 返回数组分割下标位置 - 快速排序随机化版本
+	 * @param p	排序开始位置
+	 * @param r	排序结束位置
+	 * @return
+	 */
+	public static int randomPartition(int p, int r){
+		int i = Utils.random(p, r);
+		Utils.swap(src, r, i);
+		return partition(p, r);
+	}
+	
 	public static void main(String[] args) {
 		src = Utils.random(10, 100, 10);
+		System.out.println("普通版本：");
 		Utils.print(src,"排序前");
 		int p = 0;	//起始索引,默认第一个0
 		int r = src.length - 1;	//最末索引,数组长度减一
 		sort(p, r);	//开始排序
 		Utils.print(src,"排序后");
+
+		System.out.println("随机化版本：");
+		src = Utils.random(10, 100, 10);
+		Utils.print(src,"排序前");
+		int p2 = 0;	//起始索引,默认第一个0
+		int r2 = src.length - 1;	//最末索引,数组长度减一
+		sort(p2, r2);	//开始排序
+		Utils.print(src,"排序后");	
 	}
 }
