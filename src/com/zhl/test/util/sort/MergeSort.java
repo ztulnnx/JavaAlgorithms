79dd28ee0bbddbfaf316b642d145a49593cd6b10
ztulnnx <jiabao52002443@163.com>
2021-04-23 20:49:36 +0800

    归并排序

--- src/com/zhl/test/util/sort/MergeSort.java
+++ src/com/zhl/test/util/sort/MergeSort.java
@@ -9,6 +9,18 @@ import com.zhl.test.util.Utils;
  */
 public class MergeSort {
 	
+	
+	/**
+	 * 伪代码
+	 * MERGE-SORT(A, p, r)
+	 * 	if p < r
+	 * 		q = [(p + r) / 2]
+	 * 		MERGE-SORT(A, p, q)
+	 * 		MERGE-SORT(A, q + 1, r)
+	 * 		MERGE(A, p, q, r)
+	 * 
+	 */
+	
 	/**
 	 * 归并排序 - 排序
 	 * @param src	数组
@@ -24,6 +36,29 @@ public class MergeSort {
 			mergeB(src, s, m, t);
 		}
 	}
+	
+	
+	/**
+	 * 伪代码
+	 * MERGE(A, p, q, r)
+	 * 	n1 = q - p + 1
+	 * 	n2 = r - q
+	 * 	let L[1..n1 + 1] and R[1..n2 + 1] be new arrays
+	 * 	for i = 1 to n1
+	 * 		L[i] = A[p + i - 1]
+	 * 	for j = 1 to n2
+	 * 		R[j] = A[q + j]
+	 * 	L[n1 + 1] = ∞
+	 * 	R[n2 + 1] = ∞
+	 * 	i = 1
+	 * 	j = 1
+	 * 	for k = p to r
+	 * 		if L[i] ≤ R[j]
+	 * 			A[k] = L[i]
+	 * 		else A[k] = R[j]
+	 * 			j = j + 1
+	 * 
+	 */
 	
 	/**
 	 * 归并排序 - 归并
