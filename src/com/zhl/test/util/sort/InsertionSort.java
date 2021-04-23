5cd5dd3d9830f9b74c8415fa96e4f1a6d6123abe
ztulnnx <jiabao52002443@163.com>
2021-04-23 20:23:35 +0800

    插入排序

--- src/com/zhl/test/util/sort/InsertionSort.java
+++ src/com/zhl/test/util/sort/InsertionSort.java
@@ -8,6 +8,21 @@ import com.zhl.test.util.Utils;
  *
  */
 public class InsertionSort {
+	
+	/**
+	 * 伪代码
+	 * INSERTION-SORT(A)
+	 * 	for j = 2 to A.length
+	 * 		key = A[j]
+	 * 		//Insert A[j] into the sorted sequence A[1..j-1]
+	 * 		i = j - 1
+	 * 		while i > 0 and A[i] > key
+	 * 			A[i + 1] = A[i]
+	 * 			i = i - 1
+	 * 		A[i + 1] = key
+	 * 
+	 */
+	
 	/**
 	 * 插入排序
 	 * @param src
