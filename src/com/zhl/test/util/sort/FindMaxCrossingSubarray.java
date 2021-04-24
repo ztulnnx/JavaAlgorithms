0a500cfff3058a581e04bc384f9b94f0e17c0e1f
ztulnnx <jiabao52002443@163.com>
2021-04-24 10:27:25 +0800

    求最大数组组合

--- src/com/zhl/test/util/sort/FindMaxCrossingSubarray.java
+++ src/com/zhl/test/util/sort/FindMaxCrossingSubarray.java
@@ -4,6 +4,7 @@ import com.zhl.test.util.Utils;
 
 /**
  * 求最大数组组合
+ * 	场景：给出N天的股票价格信息,算出在这N天中的哪几天买收益最大(连续)
  * @author zhanghanlin
  *
  */
@@ -12,10 +13,30 @@ public class FindMaxCrossingSubarray {
 	public static void main(String[] args) {
 		Integer[] src = Utils.random(-10, 10, 10);
 		Utils.print(src,"原始数组");
-		System.out.println("暴力解决（返回最大和）:" + maxSubSum(src, src.length - 1));
+		System.out.println("暴力解决（返回最大和）:" + maxSubSum(src));
 		SubArray subArray = findMaximumSubArray(src, 0, src.length - 1);
 		System.out.println(subArray.toString());
-	}
+	}	
+	
+	/**
+	 * 伪代码
+	 * FIND-MAXIMUM-SUMARRAY(A, low, high)
+	 * 	if high == low
+	 * 		return (lowd, high, A[low])		//base case: only one element
+	 * 	else mid = [(low + high) / 2]
+	 * 		(left-low, left-high, left-sum) = 
+	 * 			FIND-MAXIMUM-SUMARRAY(A, low, mid)
+	 * 		(right-low, right-high, right-sum) = 
+	 * 			FIND-MAXIMUM-SUMARRAY(A, mid + 1, high)
+	 * 		(cross-low, cross-high, cross-sum) = 
+	 * 			FIND-MAX-CROSSING-SUBARRAY(A, low, mid, high)
+	 * 		if left-sum ≥ right-sum and left-sum ≥ cross-sum
+	 * 			return (left-low, left-high, left-sum)
+	 * 		elseif right-sum ≥ left-sum and right-sum ≥ cross-sum
+	 * 			return (right-low, right-high, right-sum)
+	 * 		else return (cross-low, cross-high, cross-sum)	
+	 * 
+	 */
 	
 	/**
 	 * 最大子数组问题--分治策略
@@ -41,6 +62,26 @@ public class FindMaxCrossingSubarray {
 	}
 	
 	/**
+	 * 伪代码
+	 * FIND-MAX-CROSSING-SUBARRAY(A, low, mid, high)
+	 * 	left-sum = -∞
+	 * 	sum = 0
+	 * 	for i = mid downto low
+	 * 		sum = sum + A[i]
+	 * 		if sum > left-sum
+	 * 			left-sum = sum
+	 * 			max-left = i;
+	 * 	right-sum = -∞
+	 * 	sum = 0
+	 * 	for j = mid + 1 to high
+	 * 		sum = sum + A[j]
+	 * 		if sum > right-sum
+	 * 		max-right = j
+	 * 	return (max-left, max-right, left-sum + right-sum)
+	 * 
+	 */
+	
+	/**
 	 * 求跨越子数组的最大值 
 	 * @param src	数组
 	 * @param low	起始坐标
@@ -51,9 +92,9 @@ public class FindMaxCrossingSubarray {
 	public static SubArray findMaxCrossingSubarray(Integer[] src,int low,int mid,int high){
 		//sum:目前最大和	leftSum:左侧最大和	rightSum:右侧最大和
 		int sum = 0 , leftSum = 0 , rightSum = 0;
-		//左侧最大值的索引,默认起始坐标	右侧最大值索引,默认中值加一
+		//左侧最大值的索引,默认起始坐标	右侧最大值索引,默认mid加一
 		int leftIndex = low, rightIndex = mid + 1;
-		for (int i = mid; i > low; i--) {
+		for (int i = mid; i >= low; i--) {
 			sum += src[i];
 			if (sum > leftSum) {
 				leftSum = sum;
@@ -61,7 +102,7 @@ public class FindMaxCrossingSubarray {
 			}
 		}
 		sum = 0;
-		for (int i = mid + 1; i < high; i++) {
+		for (int i = mid + 1; i <= high; i++) {
 			sum += src[i];
 			if (sum > rightSum) {
 				rightSum = sum;
@@ -91,11 +132,11 @@ public class FindMaxCrossingSubarray {
 	 * @param n
 	 * @return
 	 */
-	public static int maxSubSum(Integer[] src,int n){
+	public static int maxSubSum(Integer[] src){
 		int maxSum = 0;
 		for (int i = 0; i < src.length; i++) {
 			int thisSum = 0;
-			for (int j = i; j < n; j++) {
+			for (int j = i; j < src.length; j++) {
 				thisSum += src[j];
 				if (thisSum > maxSum) {
 					maxSum = thisSum;
